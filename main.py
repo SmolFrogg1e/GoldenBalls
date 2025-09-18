8033bf32848f6d7b7280ccc9ae06484596b3c312
Cserjés Fanni Dalma <fannidalm@gmail.com>
2025-09-18 09:27:21 +0200

    balls

---
+++ main.py
@@ -0,0 +1,4 @@
+import p03
+
+p03.szamologep()
+p03.szam_bekerese(5)
---
+++ p03.py
@@ -0,0 +1,43 @@
+#3.alkalom
+#if 50>20 or 20<10:
+    #print("igaz")
+#else
+   # print("nem igaz")
+   # print("Ez is hamis ág")
+
+#print("Vége")
+#uzenet = "Hello!" if 10>1 else "bye"
+#print(uzenet)
+
+def szam_bekerese(legnagyobb_szam):
+    szam = input("Kérek egy számot:")
+    if szam.isdigit():
+        szam = int(szam)
+        if szam == 0:
+            print("Nem ismerem a 0 számot")
+        elif szam > legnagyobb_szam:
+            print("Túl nagy számot kaptam")
+        else:
+            print("Most már tudok a számmal dolgozni")
+    else:
+        print("Nem számot adtál meg!")
+    return szam
+
+def szamologep():
+    muvelet = input("Milyen műveletet akar végrehajtani (+,-,*,/):")
+    egyik_szam = szam_bekerese(10)
+    masik_szam = szam_bekerese(100)
+    if muvelet == "+":
+        eredmeny = egyik_szam + masik_szam
+    elif muvelet == "-":
+        eredmeny = egyik_szam - masik_szam
+    elif muvelet == "*":
+        eredmeny = egyik_szam * masik_szam
+    else:
+        eredmeny = egyik_szam / masik_szam
+    print(f"az eredmény: {egyik_szam} {muvelet} {masik_szam} = {eredmeny}")
+
+# A program indítása
+if __name__=="__main__":
+szamologep()
+szamologep ()
