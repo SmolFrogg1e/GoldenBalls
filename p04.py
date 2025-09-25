31792ebd7649181208180254c2b5c0080e7b64c6
Cserjés Fanni Dalma <fannidalm@gmail.com>
2025-09-25 10:18:34 +0200

    balls

--- p04.py
+++ p04.py
@@ -1,5 +1,5 @@
 # 4. alkalom
-
+'''
 def negyszog(a, b):
     ker = 2 * a + 2 * b
     ter = a * b
@@ -36,3 +36,58 @@ eredmeny = negyszog(a=4,b=4)
 print("kerület:", eredmeny[0])
 print("terület:", eredmeny[1])
 print("alakzat:", eredmeny[2])
+'''
+
+import turtle
+import random
+
+def negyzet():
+    turtle.penup()
+    turtle.goto(-50,50)
+    turtle.pendown()
+    turtle.pencolor("black")
+    turtle.pensize(5)
+
+    for _ in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+def pont(x, y):
+    turtle.goto(x, y)
+    turtle.dot(10,"black")
+
+def dobas():
+    turtle.clear()
+    turtle.hideturtle()
+    negyzet()
+    szam = random.randint(1,6)
+    turtle.penup()
+
+    if szam == 1:
+        pont(0,0)
+    elif szam == 2:
+        pont(-30, 30)
+        pont(30, -30)
+    elif szam == 3:
+        pont(-30, 30)
+        pont(30, -30)
+        pont(0, 0)
+    elif szam == 4:
+        pont(-30,30)
+        pont(30,-30)
+        pont(-30,-30)
+        pont(-30,-30)
+    elif szam == 5:
+        pont(-30,30)
+        pont(30,-30)
+        pont(-30,-30)
+        pont(-30,-30)
+        pont(0,0)
+    
+ablak = turtle.Screen()
+
+turtle.onkey(dobas,"n")
+turtle.onkey(turtle.bye,"escape")
+
+turtle.listen()
+turtle.mainloop()
